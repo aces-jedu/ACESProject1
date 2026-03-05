cf110a81449b93f7211f92f45636f390cc389883
Jedu <Jedu@DESKTOP-FQ16KU1>
2026-03-05 15:05:37 +0800

    hotfix-githubconflicts-AddInvUseCase

---
+++ ACES.UseCases/Inventories/AddInvUseCase.cs
@@ -0,0 +1,24 @@
+﻿using ACES.CoreBusiness;
+using ACES.UseCases.Inventories.Interfaces;
+using ACES.UseCases.PluginInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ACES.UseCases.Inventories
+{
+    public class AddInvUseCase : IAddInvUseCase
+    {
+        private readonly IInvRepositories invRepositories;
+
+        public AddInvUseCase(IInvRepositories invRepositories)
+        {
+            this.invRepositories = invRepositories;
+        }
+        public async Task ExecuteAsync(Inventory inventory)
+        {
+            await this.invRepositories.AddInvAsync(inventory);
+        }
+
+    }
+}
--- ACES.UseCases/PluginInterfaces/IInvReporsitories.cs
+++ ACES.UseCases/PluginInterfaces/IInvReporsitories.cs
@@ -7,6 +7,7 @@ namespace ACES.UseCases.PluginInterfaces
 {
     public interface IInvReporsitories
     {
+        Task AddInvAsync(Inventory inventory);
         Task <IEnumerable<Inventory>>GetInvbyNameAsync(string name);
     }
 }
